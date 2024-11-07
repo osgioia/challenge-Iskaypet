62fb89fe3a50c8f344486d8513451a1233214d84
Osvaldo Gioia <os.gioia@gmail.com>
2024-11-07 18:21:25 -0300

    fix: unit test client

--- go.mod
+++ go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- handlers/client_handler_test.go
+++ handlers/client_handler_test.go
@@ -5,14 +5,28 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
+	"golangApp/config"
 	"golangApp/models"
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
+
+func setupTestDB() {
+	var err error
+	config.DB, err = gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	config.DB.AutoMigrate(&models.Client{})
+}
 
 func TestValidEmail(t *testing.T) {
 	tests := []struct {
@@ -146,22 +160,17 @@ func TestCreateClientValidation(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// Serializamos el cliente como JSON
 			jsonBytes, err := json.Marshal(tt.client)
 			assert.NoError(t, err)
 
-			// Configuramos el request y el contexto
 			req := httptest.NewRequest(http.MethodPost, "/api/v1/clients", bytes.NewBuffer(jsonBytes))
 			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
 
-			// Llamamos a la función CreateClient
 			if assert.NoError(t, CreateClient(c)) {
-				// Verificamos el código de estado
 				assert.Equal(t, http.StatusBadRequest, rec.Code)
 
-				// Verificamos el contenido del JSON de error
 				var resp map[string]string
 				err := json.Unmarshal(rec.Body.Bytes(), &resp)
 				assert.NoError(t, err)
@@ -172,10 +181,8 @@ func TestCreateClientValidation(t *testing.T) {
 }
 
 func TestCalculateClientKPI(t *testing.T) {
-	// Test data
 	ages := []float64{20, 30, 40}
 
-	// Calculate expected values
 	var sum float64
 	for _, age := range ages {
 		sum += age
@@ -191,7 +198,242 @@ func TestCalculateClientKPI(t *testing.T) {
 		expectedStdDev = float64(varianceSum / float64(len(ages)))
 	}
 
-	// Verify calculations
 	assert.Equal(t, float64(30), expectedAverage)
 	assert.InDelta(t, float64(66.66666666666667), expectedStdDev, 0.000001)
+}
+
+func TestGetAll(t *testing.T) {
+	setupTestDB()
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/clients/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	clients := []models.Client{
+		{
+			ID:        1,
+			Name:      "John",
+			LastName:  "Doe",
+			Email:     "john.doe@example.com",
+			BirthDay:  time.Date(1990, time.January, 1, 0, 0, 0, 0, time.UTC),
+			Age:       33,
+			Telephone: "123456789",
+		},
+		{
+			ID:        2,
+			Name:      "Jane",
+			LastName:  "Smith",
+			Email:     "jane.smith@example.com",
+			BirthDay:  time.Date(1985, time.February, 14, 0, 0, 0, 0, time.UTC),
+			Age:       39,
+			Telephone: "987654321",
+		},
+		{
+			ID:        3,
+			Name:      "Alice",
+			LastName:  "Johnson",
+			Email:     "alice.johnson@example.com",
+			BirthDay:  time.Date(1995, time.March, 30, 0, 0, 0, 0, time.UTC),
+			Age:       29,
+			Telephone: "555555555",
+		},
+	}
+
+	for _, client := range clients {
+		config.DB.Create(&client)
+	}
+
+	err := GetAll(c)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		expectedResponse := `[
+			{
+				"id": 1,
+				"name": "John",
+				"last_name": "Doe",
+				"email": "john.doe@example.com",
+				"birth_day": "1990-01-01T00:00:00Z",
+				"age": 33,
+				"Telephone": "123456789"
+			},
+			{
+				"id": 2,
+				"name": "Jane",
+				"last_name": "Smith",
+				"email": "jane.smith@example.com",
+				"birth_day": "1985-02-14T00:00:00Z",
+				"age": 39,
+				"Telephone": "987654321"
+			},
+			{
+				"id": 3,
+				"name": "Alice",
+				"last_name": "Johnson",
+				"email": "alice.johnson@example.com",
+				"birth_day": "1995-03-30T00:00:00Z",
+				"age": 29,
+				"Telephone": "555555555"
+			}
+		]`
+		assert.JSONEq(t, expectedResponse, rec.Body.String())
+	}
+
+	config.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&models.Client{})
+}
+
+func TestGetClient(t *testing.T) {
+	setupTestDB()
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/clients/10", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetParamNames("id")
+	c.SetParamValues("10")
+
+	testClient := models.Client{
+		ID:        10,
+		Name:      "John",
+		LastName:  "Doe",
+		Email:     "john.doe@example.com",
+		BirthDay:  time.Date(1990, time.January, 1, 0, 0, 0, 0, time.UTC),
+		Age:       33,
+		Telephone: "123456789",
+	}
+
+	config.DB.Create(&testClient)
+
+	err := GetClient(c)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		expectedResponse := `{
+			"id": 10,
+			"name": "John",
+			"last_name": "Doe",
+			"email": "john.doe@example.com",
+			"birth_day": "1990-01-01T00:00:00Z",
+			"age": 33,
+			"Telephone": "123456789"
+		}`
+		assert.JSONEq(t, expectedResponse, rec.Body.String())
+	}
+
+	config.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&models.Client{})
+}
+
+func TestGetClientNotFound(t *testing.T) {
+	setupTestDB()
+
+	e := echo.New()
+
+	req := httptest.NewRequest(http.MethodGet, "/clients/999", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	c.SetParamNames("id")
+	c.SetParamValues("999")
+
+	err := GetClient(c)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusNotFound, rec.Code)
+
+		var jsonErr map[string]string
+		json.Unmarshal(rec.Body.Bytes(), &jsonErr)
+		assert.Equal(t, "Client not found", jsonErr["error"])
+	}
+}
+
+func TestUpdateClient(t *testing.T) {
+	setupTestDB()
+
+	e := echo.New()
+	originalClient := models.Client{
+		ID:        1,
+		Name:      "John",
+		LastName:  "Doe",
+		Email:     "john.doe@example.com",
+		BirthDay:  time.Date(1990, time.January, 1, 0, 0, 0, 0, time.UTC),
+		Age:       33,
+		Telephone: "123456789",
+	}
+
+	config.DB.Create(&originalClient)
+
+	updateData := `{
+		"name": "Johnny",
+		"last_name": "Smith",
+		"email": "johnny.smith@example.com",
+		"age": 34,
+		"Telephone": "987654321"
+	}`
+
+	req := httptest.NewRequest(http.MethodPut, "/clients/1", strings.NewReader(updateData))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+
+	err := UpdateClient(c)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		expectedResponse := `{
+			"id": 1,
+			"name": "Johnny",
+			"last_name": "Smith",
+			"email": "johnny.smith@example.com",
+			"birth_day": "1990-01-01T00:00:00Z",
+			"age": 34,
+			"Telephone": "987654321"
+		}`
+		assert.JSONEq(t, expectedResponse, rec.Body.String())
+	}
+
+	config.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&models.Client{})
+}
+
+func TestDeleteClient(t *testing.T) {
+	setupTestDB()
+
+	e := echo.New()
+
+	testClient := models.Client{
+		ID:        1,
+		Name:      "John",
+		LastName:  "Doe",
+		Email:     "john.doe@example.com",
+		BirthDay:  time.Date(1990, time.January, 1, 0, 0, 0, 0, time.UTC),
+		Age:       33,
+		Telephone: "123456789",
+	}
+
+	config.DB.Create(&testClient)
+
+	req := httptest.NewRequest(http.MethodDelete, "/clients/1", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+
+	err := DeleteClient(c)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusNoContent, rec.Code)
+
+		var deletedClient models.Client
+		err := config.DB.First(&deletedClient, 1).Error
+		assert.Error(t, err)
+	}
+
+	config.DB.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&models.Client{})
 }
